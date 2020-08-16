fc34baa9832eac379519a12d499c67d13b4cb0dd
rafayetn <rafayet@nimblehq.co>
2020-08-16 16:45:56 +0600

    Edit 4

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,13 @@
-import {message, danger} from "danger"
-import eslint from 'danger-plugin-eslint'
-import stylelint from 'danger-plugin-stylelint'
-import jest from 'danger-plugin-jest'
+const {danger, fail, message, warn, markdown} =  require("danger")
 
-jest()
-eslint()
-stylelint()
+const fs = require("fs")
+
+const testFile = "eslint-output.log"
+const linterOutput = fs.readFileSync(testFile).toString()
+
+if (!linterOutput.includes('0 Errors')) {
+  fail('ES Lint did not pass');
+  markdown(`These changes failed to pass the linter:
+${linterOutput}
+  `)
+}
