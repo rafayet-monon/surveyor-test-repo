d3ca8476eb3b875209fa9ecc49d30d5ef91b154e
rafayetn <rafayet@nimblehq.co>
2020-08-16 17:14:04 +0600

    Edit 7

--- dangerfile.js
+++ dangerfile.js
@@ -6,13 +6,13 @@ const fs = require("fs")
 // message("This is a normal message")
 // markdown("*Markdown* is also **supported**")
 
-const { additions = 0, deletions = 0 } = danger.github.pr
-message(`:tada: The PR added ${additions} and removed ${deletions} lines.`)
+// const { additions = 0, deletions = 0 } = danger.github.pr
+// message(`:tada: The PR added ${additions} and removed ${deletions} lines.`)
 
 const testFile = "eslint-output.log"
 const linterOutput = fs.readFileSync(testFile).toString()
 
-if (!linterOutput.includes('0 Errors')) {
+if (!linterOutput.includes('Error')) {
   fail('ES Lint did not pass');
   markdown(`These changes failed to pass the ES Lint:
 ${linterOutput}
