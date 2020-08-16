96944839ba571304a44e68a8f4779660fe9dee7d
rafayetn <rafayet@nimblehq.co>
2020-08-16 16:53:43 +0600

    Edit 5

--- dangerfile.js
+++ dangerfile.js
@@ -1,13 +1,20 @@
 const {danger, fail, message, warn, markdown} =  require("danger")
-
 const fs = require("fs")
+
+fail("This is a failure message")
+warn("This is a warning")
+message("This is a normal message")
+markdown("*Markdown* is also **supported**")
+
+const { additions = 0, deletions = 0 } = danger.github.pr
+message(`:tada: The PR added ${additions} and removed ${deletions} lines.`)
 
 const testFile = "eslint-output.log"
 const linterOutput = fs.readFileSync(testFile).toString()
 
 if (!linterOutput.includes('0 Errors')) {
-  fail('ES Lint did not pass');
-  markdown(`These changes failed to pass the linter:
+  // fail('ES Lint did not pass');
+  fail(`These changes failed to pass the linter:
 ${linterOutput}
   `)
 }
