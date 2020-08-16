aee2ff9fa5b1e6378c6fcaabdc8fe42b23a51bbb
rafayetn <rafayet@nimblehq.co>
2020-08-16 17:45:51 +0600

    Edit 8

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,7 @@
 const {danger, fail, message, warn, markdown} =  require("danger")
 const fs = require("fs")
+const jest = require('danger-plugin-jest/dist/index.js')
+const path = require('path')
 
 // fail("This is a failure message")
 // warn("This is a warning")
@@ -8,13 +10,10 @@ const fs = require("fs")
 
 // const { additions = 0, deletions = 0 } = danger.github.pr
 // message(`:tada: The PR added ${additions} and removed ${deletions} lines.`)
-
+jest.default();
 const testFile = "eslint-output.log"
 const linterOutput = fs.readFileSync(testFile).toString()
 
 if (linterOutput.includes('Error')) {
   fail('ES Lint did not pass');
-  markdown(`These changes failed to pass the ES Lint:
-${linterOutput}
-  `)
 }
